3dcbf51ec2a1bb07422899112a61f4113687968c
Xander <j-xander@users.noreply.github.com>
2017-12-10 22:37:49 -0500

    Update take_multiple_pictures.py

--- examples/take_multiple_pictures.py
+++ examples/take_multiple_pictures.py
@@ -4,11 +4,16 @@
 from picamera import PiCamera
 from optparse import OptionParser
 
-parser = OptionParser()
-usage = "usage: %prog [options]"
-parser.add_option("-n", "--number", type="string", help="supply number of pictures to take", dest="captures", default=1)
-
-# creates a camera object to take pictures or videos with
+# declares objects to use later
 camera = PiCamera()
+parser = OptionParser()
 
-camera.capture_sequence(['/home/pi/image%s.jpg' % i for i in range(captures)])
+# gives usage information and adds options for script
+usage = "usage: %prog [options]"
+parser.add_option("-n", "--number", help="supply number of pictures to take", dest="captures", default=1)
+
+# parses the arguments and stores them in tuples
+(opts, args) = parser.parse_args()
+
+# captures images in a sequence based on a user supplied value
+camera.capture_sequence(['/home/pi/image%s.jpg' % i for i in range(int(captures))])
